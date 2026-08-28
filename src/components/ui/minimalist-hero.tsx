145215e0ddc6d0626281fc367621bee7d2ef5141
Arisio Andrade <156484381+arisioandradee@users.noreply.github.com>
2026-08-28 17:33:38 -0300

    Update minimalist-hero.tsx

--- src/components/ui/minimalist-hero.tsx
+++ src/components/ui/minimalist-hero.tsx
@@ -75,7 +75,7 @@ export const MinimalistHero = ({
              loop
              muted
              playsInline
-             className="absolute top-0 left-0 right-0 z-0 h-[50vh] md:inset-0 md:h-full w-full object-cover object-[25%_10%] md:object-[center_10%]"
+             className="absolute top-0 left-0 right-0 z-0 h-[50vh] md:inset-0 md:h-full w-full object-cover object-[50%_10%] md:object-[75%_10%]"
            >
              <source src={videoSrc} type="video/mp4" />
            </video>
